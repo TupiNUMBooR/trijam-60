c4213d5633539da681cfee4bd577152111e5513e
TupiNUMBooR <TupiNUMBooR@ya.ru>
2020-03-08 04:42:34 +0300

    Add build script and empty folders

---
+++ Assets/Editor/Builder.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class Builder
+    {
+        public static void BuildDefault()
+        {
+            Build("Windows64", BuildTarget.StandaloneWindows64, false);
+            Build("Linux64", BuildTarget.StandaloneLinux64, false);
+            Build("WebGL", BuildTarget.WebGL, false);
+        }
+
+        static void Build(string platform, BuildTarget bt, bool dev)
+        {
+            var l = (from scene in EditorBuildSettings.scenes where scene.enabled select scene.path).ToArray();
+            BuildPipeline.BuildPlayer(l, Path(dev ? "Dev" : "", platform), bt, dev ? BuildOptions.Development : BuildOptions.None);
+        }
+
+        static string Path(string type, string platform)
+        {
+            return $"Builds/Basic{type}/{platform}/{Application.productName}/{Application.productName}";
+        }
+    }
+}
